370dd1d9af1ea16a924b028234b434ae4d67598d
zylina <zylina.lion@gmail.com>
2020-10-13 21:05:47 -0400

    updated PetTest.java

--- spring-petclinic-customers-service/src/test/java/org/model/PetTest.java
+++ spring-petclinic-customers-service/src/test/java/org/model/PetTest.java
@@ -20,7 +20,7 @@ public class PetTest {
         // Arrange
         Pet pet = new Pet();
         Owner owner = new Owner();
-        owner.setFirstName("Call");
+        owner.setFirstName("Call1");
         // Act
         pet.setOwner(owner);
         // Assert
